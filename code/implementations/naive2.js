d3cae7dc79bcd5f3370227f9bae0c13d1082f8eb
andrew hutchison <drew.hutchison@nimbusnine.co>
2015-08-03 17:58:28 -0600

    reordered if block

--- code/implementations/naive2.js
+++ code/implementations/naive2.js
@@ -7,12 +7,13 @@ var solution = function(pirates) {
 
   for (var i=0; i<=pirates.length; i++) {
 
-    if (spoken.indexOf(current_pirate) != -1) {
-      // if we've talked to current_pirate already
-      return spoken.length;
-    } else {
+    if (spoken.indexOf(current_pirate) == -1) {
+      // we haven't talked to current_pirate yet
       spoken.push(current_pirate);
       current_pirate = pirates[current_pirate];
+    } else {
+      // otherwise, we're done
+      return spoken.length;
     };
 
   };
